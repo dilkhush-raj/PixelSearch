f4578435d1e705003b965046e3caed8df6c120bc
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-05-29 15:59:32 +0530

    remove deleted import

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Home, SearchImage } from './pages';
-import Layout from './layout/Layout';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home, SearchImage } from "./pages";
+import Layout from "./layout/Layout";
+import "./index.css";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Route, Router, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import SearchImageTwo from './pages/SearchImageTwo';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  Route,
+  Router,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />} >
-      <Route path='' element={<Home />} />
-      <Route path='/image' element={<SearchImage />} />
-      <Route path='/picture' element={<SearchImageTwo />} />
-      </Route>
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="/image" element={<SearchImage />} />
+    </Route>
   )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
